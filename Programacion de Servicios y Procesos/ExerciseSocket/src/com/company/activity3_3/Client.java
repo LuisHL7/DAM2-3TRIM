4c805b307a081359bdc236df3ed02c160c42d5ae
Luis Joseph Huapaya Liñan <a19luisjhl@ATEIS.LOCAL>
2022-02-25 18:16:30 +0100

    save

---
+++ Programacion de Servicios y Procesos/ExerciseSocket/src/com/company/activity3_3/Client.java
@@ -0,0 +1,28 @@
+package com.company.activity3_3;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        int port = 6000;
+        ServerSocket server = new ServerSocket(port);
+        Socket clientConnect = null;
+        System.out.println("Waiting th client");
+        clientConnect = server.accept();
+
+        //Input
+        InputStream input = clientConnect.getInputStream();
+        DataInputStream dataI = new DataInputStream(input);
+        System.out.println("Recep the client: \n\t" + data.readUTF());
+
+        //Output
+        OutputStream output = clientConnect.getOutputStream();
+        DataOutputStream dataO = new DataOutputStream(output);
+        dataO.writeUTF("HELLO CLIENT");
+
+
+
+    }
+}
---
+++ Programacion de Servicios y Procesos/ExerciseSocket/src/com/company/activity3_3/Server.java
@@ -0,0 +1,7 @@
+package com.company.activity3_3;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
