59d0726867ba8a80ac5cc510eb73bd9913517c28
Luis Joseph Huapaya Liñan <a19luisjhl@ateis.local>
2022-04-06 20:55:30 +0200

    accountant finished

--- Programacion Movil/Exercises/AccountantByViewModel/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,15 +0,0 @@
-
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-g
-7app/src/main/java/com/example/mainactivity/ViewModel.kt,9\2\924d073efc7938e82d2112598b4d75be21d84d37
-c
-3app/src/main/java/com/example/mainactivity/Model.kt,1\2\12da353ed8c9c3a4e43642593a3d2daeb94f2cf8
-?
-settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
-@
-app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-j
-:app/src/main/java/com/example/mainactivity/MainFragment.kt,8\e\8e76f6993645cb9b35fd896efadbeacf78cd28a0
-j
-:app/src/main/java/com/example/mainactivity/MainActivity.kt,3\a\3a8988857fb5b1c718d5829c2acdd06c37e8267a
--- Programacion Movil/Exercises/AccountantByViewModel/app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ Programacion Movil/Exercises/AccountantByViewModel/app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -2,22 +2,14 @@ package com.example.mainactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import com.example.mainactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: ViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val navController = findNavController(R.id.)
-        binding.TxTNumber.text = viewModel.stateCount().toString()
-
-        binding.BtnCount.setOnClickListener {
-            binding.TxTNumber.text = viewModel.increaseCount().toString()
-        }
     }
 }
