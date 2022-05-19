7c5741d85e26df308910997af6b9e17d096d24ce
Luis Huapaya <luisjosephnb28@gmail.com>
2022-05-20 00:26:27 +0200

    base-proyect

---
+++ Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/androidTest/java/com/example/huapaya_luis_examen2t/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.huapaya_luis_examen2t
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.huapaya_luis_examen2t", appContext.packageName)
+    }
+}
---
+++ Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.huapaya_luis_examen2t
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
