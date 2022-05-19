e16cf079467f313a41504ce752fe95fd19d1bad2
Luis Huapaya <luisjosephnb28@gmail.com>
2022-05-20 01:26:43 +0200

    Bottom finished

--- Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/MainActivity.kt
+++ Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.huapaya_luis_examen2t
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.huapaya_luis_examen2t.bottomNav.BottonNavigation
+import com.example.huapaya_luis_examen2t.databinding.ActivityMainBinding
+import com.example.huapaya_luis_examen2t.drawerNav.NavigationDrawerActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.BtnBottomNav.setOnClickListener {
+            startActivity(Intent(this, BottonNavigation::class.java))
+        }
+        binding.BtnDrawerNav.setOnClickListener {
+            startActivity(Intent(this, NavigationDrawerActivity::class.java))
+        }
     }
 }
---
+++ Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/bottomNav/BottonNavigation.kt
@@ -0,0 +1,57 @@
+package com.example.huapaya_luis_examen2t.bottomNav
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.huapaya_luis_examen2t.R
+import com.example.huapaya_luis_examen2t.databinding.ActivityBottonNavigationBinding
+import com.google.android.material.snackbar.Snackbar
+
+class BottonNavigation : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBottonNavigationBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBottonNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_botton_navigation) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.navigation_drawer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_message ->
+                Snackbar.make(binding.root, navController.currentDestination?.label ?: "No tiene título", Snackbar.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
