e27cd875ba6f3e8a03656cd5d5a54b970264bf7f
Luis Huapaya <luisjosephnb28@gmail.com>
2022-05-21 19:28:20 +0200

    save

--- Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/drawerNav/ui/ganador/GanadorFragment.kt
+++ Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/drawerNav/ui/ganador/GanadorFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.huapaya_luis_examen2t.databinding.FragmentGanadorBinding
 
 class GanadorFragment : Fragment() {
 
     private var _binding: FragmentGanadorBinding? = null
     private val binding get() = _binding!!
+    private val args: GanadorFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +21,13 @@ class GanadorFragment : Fragment() {
     ): View {
         _binding = FragmentGanadorBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.TxtGanador.visibility = View.VISIBLE
+        binding.Txtjugador.visibility = View.VISIBLE
+        binding.Txtjugador.text = args.jugador
     }
 
     override fun onDestroyView() {
--- Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/drawerNav/ui/tresenraya/TresEnRayaFragment.kt
+++ Programacion Movil/Navegacion/Exercises/PracticandoExamen2T/app/src/main/java/com/example/huapaya_luis_examen2t/drawerNav/ui/tresenraya/TresEnRayaFragment.kt
@@ -1,17 +1,22 @@
 package com.example.huapaya_luis_examen2t.drawerNav.ui.tresenraya
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.GridLayout
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.huapaya_luis_examen2t.databinding.FragmentTresenrayaBinding
 
 class TresEnRayaFragment : Fragment() {
 
     private var _binding: FragmentTresenrayaBinding? = null
     private val binding get() = _binding!!
+    private val TAG = TresEnRayaFragment::class.java.name
+    private lateinit var modelo: Tablero
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,9 +30,51 @@ class TresEnRayaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.BtnEmpezar.setOnClickListener {
-            Navigation.findNavController(view).navigate(TresEnRayaFragmentDirections.actionNavTresenrayaToNavGanador())
+        binding.buttonGrid.setOnClickListener(::onCellClicked) // Asignamos comportamiento a los botones
+        modelo = Tablero() // Instanciamos el modelo en el momento en que se lanza la activity
+
+        binding.BtnReiniciar.setOnClickListener { reset() }
+
+    }
+
+
+    /** Método que se lanza cuando se hace click en una celda */
+    private fun onCellClicked(button: Button) {
+        val tag = button.tag.toString().toCharArray()
+        val row = tag[0].digitToInt()
+        val col = tag[1].digitToInt()
+        val jugadorQueMovio = modelo.marcar(row, col) ?: return // TODO: arreglar con scoped functions?
+
+        Log.i(TAG, "Celda [$row,$col] marcada por Jugador $jugadorQueMovio")
+        binding.BtnReiniciar.visibility = View.VISIBLE
+
+        button.text = jugadorQueMovio.toString()
+
+        if (modelo.ganador != null) { // Comprobamos si el movimiento ha generado un ganador
+            findNavController().navigate(TresEnRayaFragmentDirections.actionNavTresenrayaToNavGanador(jugadorQueMovio.toString()))
         }
+
+    }
+
+    /** Función de extensión que recibe como parámetro una lambda
+     * Se exitende GridLayout para que tenga este método, el cual recorre todos los
+     * Button que la componen (asunción) y para cada uno de ellos le asigna la función
+     * pasada a su OnClickListener */
+    private fun GridLayout.setOnClickListener( onClick: (Button) -> Unit ) {
+        for (i in 0 until childCount) {
+            val boton = getChildAt(i) as Button
+            boton.setOnClickListener {
+                onClick(boton)
+            }
+        }
+    }
+
+    private fun reset() {
+        modelo.reiniciar()
+        for (i in 0 until binding.buttonGrid.childCount) {
+            (binding.buttonGrid.getChildAt(i) as Button).text = null
+        }
+        binding.BtnReiniciar.visibility = View.GONE
     }
 
     override fun onDestroyView() {
@@ -35,3 +82,5 @@ class TresEnRayaFragment : Fragment() {
         _binding = null
     }
 }
+
+
